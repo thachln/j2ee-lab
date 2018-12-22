e19b3a11090738f57f5d3ce7bdbab1b82cc82372
ThachLN <ThachLN@gmail.com>
2018-12-22 15:10:54 +0700

    Create Springboot by Eclipse + plugin SpringToolSuite. Delete from unneeded folders/files.

---
+++ Springboot/se-summary/src/main/java/mksgroup/springboot/SeSummaryApplication.java
@@ -0,0 +1,14 @@
+package mksgroup.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeSummaryApplication.class, args);
+	}
+
+}
+
